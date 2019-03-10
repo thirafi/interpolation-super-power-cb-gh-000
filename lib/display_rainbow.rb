2db6f61c80fc27894f9e0211b8d77f0b8ef2d6e2
thirafi aufar idris <thirafi_aja@apps.ipb.ac.id>
2019-03-10 04:19:19 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,7 @@
 # Write your #display_rainbow method here
 def  display_rainbow(colors)
   letters = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
-  puts colors[0].upcase
+  puts colors[][0].upcase
 end
+
+display_rainbow(["red","blue"])

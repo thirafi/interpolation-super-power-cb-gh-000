007aee163a12479054314a1ca84019a9217d21ba
thirafi aufar idris <thirafi_aja@apps.ipb.ac.id>
2019-03-10 04:29:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,5 @@
 # Write your #display_rainbow method here
 def  display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-    colors =
   puts  "The first letter in the alphabet is: #{colors}"
 end
 

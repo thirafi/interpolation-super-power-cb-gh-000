d7b46ac678f61ca8f2d8bba058eddd39ccc7730b
thirafi aufar idris <thirafi_aja@apps.ipb.ac.id>
2019-03-10 04:36:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def  display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   letter = ['R', 'O', 'Y', 'G', 'B', 'I', 'V']
-  puts  colors
+  puts  colors .+.letter
 end
 
 display_rainbow(["red","blue"])
